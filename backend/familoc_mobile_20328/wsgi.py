2cfb499f76705107035c043919362971185a7fd3
Crowdbotics <team@crowdbotics.com>
2020-09-16 07:46:08 +0000

    Update models

--- backend/familoc_mobile_20328/wsgi.py
+++ backend/familoc_mobile_20328/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'familoc_mobile_20328.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "familoc_mobile_20328.settings")
 
 application = get_wsgi_application()
